fbf3d3e67946e91ed83f2a6c9f196aabbbcfe089
Ammar Dodin <azdodin@ucdavis.edu>
2021-02-21 18:24:02 -0500

    cleanup solution
    previous solution wasn't correct. Didn't handle null left or right

--- 20-02-2021/solution.py
+++ 20-02-2021/solution.py
@@ -9,22 +9,37 @@ class Node:
     def is_leaf(self):
         return (self is not None
                 and self.left == self.right == None)
+    
+    def equal_to_left_and_right(self):
+        if not self.left and not self.right:
+            return True
+        if self.left and self.right and self.val == self.right.val == self.left.val:
+            return True
+        if self.left:
+            return self.val == self.left.val
+        if self.right:
+            return self.val == self.right.val
 
 def count_unival_subtrees(root):
-    if not root:
-        return 0
-    if root.is_leaf():
-        return 1
-    left_count = count_unival_subtrees(root.left)
-    right_count = count_unival_subtrees(root.right)
-    total = left_count + right_count
-    if root.val == root.left.val == root.right.val:
-        total += 1
-    return total
+    def solve(root):
+        if not root:
+            return 0, True
+        left_count, left_is_unival = solve(root.left)
+        right_count, right_is_unival = solve(root.right)
+        root_is_unival = left_is_unival and right_is_unival and root.equal_to_left_and_right()
+        total = left_count + right_count
+        if root_is_unival:
+            return total + 1, True
+        return total, False
+    num_subtrees, _ = solve(root)
+    return num_subtrees
+
 
 assert count_unival_subtrees(None) == 0
 assert count_unival_subtrees(Node(1)) == 1
 assert count_unival_subtrees(Node(0)) == 1
 assert count_unival_subtrees(Node(1, Node(1), Node(1))) == 3
 assert count_unival_subtrees(Node(1, Node(0), Node(1))) == 2
-assert(count_unival_subtrees(Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0))))) == 5
+assert count_unival_subtrees(Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0)))) == 5
+assert count_unival_subtrees(Node(5, Node(1, Node(5), Node(5)), Node(5, None, Node(5)))) == 4
+assert count_unival_subtrees(Node(1, Node(3, Node(4), Node(5)), Node(2, None, Node(6)))) == 3
